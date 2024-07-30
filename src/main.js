830d3e792309590495bebbf42e54cfd9f9bf9b99
Roksana St <160417317+Roksana-St@users.noreply.github.com>
2024-07-30 11:18:25 +0300

    upgrade

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import { fetchImages } from './js/pixabay-api.js';
 import { renderImages, clearGallery } from './js/render-functions.js';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.form');
   const input = form.querySelector('input[name="query"]');
-  const loader = document.querySelector('.loader');
+    const loader = document.querySelector('.loader');
+    
+    
+    
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -38,9 +43,14 @@ form.addEventListener('submit', (event) => {
             }
 
             renderImages(data.hits);
+            let lightbox = new SimpleLightbox('.gallery a');
             lightbox.refresh();
         })
         .catch(error => {
+            iziToast.error({
+                title: 'Error',
+                        message: 'Failed to fetch images. Please try again later.',
+                    })
             loader.classList.add('hidden');
             console.error('Error fetching images:', error);
         });
